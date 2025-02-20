30c0a47b80268e3adaec6e760b486459357b1396
Alejandro Herrera <alejocorella@gmail.com>
2025-02-19 19:56:51 -0600

    Added new functionality to be able to add friends to the list to be sorted also added functionality to capitalize all the names if they are not. Second commit

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+let textoIngresado=" ";
+
+function nombreMayuscula(nombreIngresado){
+    return nombreIngresado
+    .toLowerCase()
+    .split(" ")
+    .map(nombreSolo=>nombreSolo.charAt(0).toUpperCase()+nombreSolo.slice(1))
+    .join(" ");
+};
+function agregarAmigo(){
+    let nuevoAmigo="";
+    textoIngresado=document.getElementById("amigo").value;
+    if (textoIngresado!==""){
+        console.log(`New: ${textoIngresado}`);
+        nuevoAmigo=nombreMayuscula(textoIngresado);
+        amigos.push(nuevoAmigo);
+        document.getElementById("listaAmigos").innerHTML+=`<p>${nuevoAmigo}</p>`;
+        console.log(amigos);
+        document.getElementById("amigo").value="";
+    }else{
+        alert("Ingresaste un Nombre Invalido");
+    };
+};
