8f61c2f0f7e0e52a42cf3c8fb4347f4e66bedceb
Alejandro Herrera <alejocorella@gmail.com>
2025-02-20 20:33:42 -0600

    Added functionality to reset game and randomly select a friend added function to reset the game entering a new name

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let amigos=[];
-let textoIngresado=" ";
+let textoIngresado="";
+let amigoSorteado=false;
 
+function reiniciarJuego(){
+    document.getElementById("listaAmigos").innerHTML="";
+    amigos=[];
+};
 function nombreMayuscula(nombreIngresado){
     return nombreIngresado
     .toLowerCase()
@@ -11,14 +16,24 @@ function nombreMayuscula(nombreIngresado){
 function agregarAmigo(){
     let nuevoAmigo="";
     textoIngresado=document.getElementById("amigo").value;
+    if (amigoSorteado==true){
+        reiniciarJuego();
+        amigoSorteado=false;
+    };
     if (textoIngresado!==""){
         console.log(`New: ${textoIngresado}`);
         nuevoAmigo=nombreMayuscula(textoIngresado);
         amigos.push(nuevoAmigo);
-        document.getElementById("listaAmigos").innerHTML+=`<p>${nuevoAmigo}</p>`;
         console.log(amigos);
+        document.getElementById("listaAmigos").innerHTML+=`<li>${nuevoAmigo}</li>`;
         document.getElementById("amigo").value="";
     }else{
         alert("Ingresaste un Nombre Invalido");
     };
+};
+function sortearAmigo(){
+    const indiceAmigo=Math.floor(Math.random()*amigos.length);
+    console.log(`Amigo secreto: ${amigos[indiceAmigo]}`);
+    document.getElementById("listaAmigos").innerHTML=`<li>El Amigo Secreto es:</li><li>${amigos[indiceAmigo]}</li>`;
+    amigoSorteado=true;
 };
